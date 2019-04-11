b7901083a38578446e2ec774f153cbee53a5e90f
Scott Yanco <scott.yanco@ucdenver.edu>
2019-04-11 14:44:29 -0600

    Added data folder to .gitignore. New function in iterate iso that iterates model across differnt samples sizes for source iso distribution estimation.

--- R/iterateiso.R
+++ R/iterateiso.R
@@ -1,20 +1,43 @@
-iterateiso <- function (num_individ, steps, num_sources, diet_prop, food,
-                        popsize, mu_carb = NULL, sd_carb = NULL, mu_nit = NULL,
-                        sd_nit = NULL){
+iteratesourcesamp <- function (folder, num_sources, popsize, mu_carb, sd_carb,
+                               mu_nit, sd_nit, sourcesamp, num_individ, steps,
+                               diet_prop, window){
+  wd <- getwd()#get current wd
+  #change working directory to designated data folder
+  setwd(folder)
 
+  #simulate prey sources
+  preysource <- makefood(num_sources = num_sources, popsize = popsize,
+                   mu_carb = mu_carb, sd_carb = sd_carb, mu_nit = mu_nit,
+                   sd_nit = sd_nit)
 
+  #sample from pery source populations walking across parameter space of sample
+  #sizes.  Writes source iso estimates to file
+  for(i in sourcesamp){
+    samplesources(num_samples = i, food = preysource,
+                  filename = paste0("simulated_source_iso_n", i,
+                                    ".csv"))
   }
+  #
+  #
+  # sapply(sourcesamp, FUN = samplesources, food = preysource,
+  #        filename = paste0("simulated_source_iso_n", sourcesamp, ".csv"))
 
-makeparameterspace <- function(carbon = T, nitrogen = T, sources){
-  range1 <- c(-20, -1)
-  interval1 <- 1
-  one <- as.factor(seq(range1[1], range1[2], by = interval1))
+  #save isotopic discrimination factors to file
+  savediscrimination(food = preysource, filename="simulated_discrimination.csv")
 
-  range2 <- c(-10, -1)
-  interval2 <- 1
-  two <- seq(range2[1], range2[2], by = interval2)
+  #simulate agents over time
+  specimens <- eatfood(num_individ =  num_individ, steps = steps,
+                       num_sources = num_sources, diet_prop = diet_prop,
+                       food = preysource)
 
-  range3 <- c(-100, -50)
-  interval3 <- 1
-  three <- seq(range3[1], range3[2], by = interval3)
+  #collect "observed" isotope values
+  obs_iso <- getiso(food = specimens, time = steps, window = window)
+
+  #format to data frame and save as csv for import to MixSIAR
+  formatiso(iso = obs_iso, filename = "simulated_consumer_iso.csv")
+
+  #return wd to previous
+  setwd(wd)
 }
+
+
