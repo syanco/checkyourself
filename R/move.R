b6f38042ec1b2800db69b627d1e501a7e6212c52
Scott Yanco <scott.yanco@ucdenver.edu>
2019-09-23 20:39:23 -0600

    Updated documentation of functions in move.R

--- R/move.R
+++ R/move.R
@@ -1,5 +1,34 @@
-#overall location selection function
-choose.loc <- function(r, pd.rate, steps, lambda, coef.d, coef.r, blank.rast, matsize, ...) {
+#' chooseLoc
+#'
+#'
+#' A function to simulate a single individual moving with varying mixtures of
+#' preference for habitat and/or short steps.
+#'
+#' @param hab.prob matrix, the habitat matrix with cell values representing the
+#' habitat-only probabilities (uncorrected for the mixing proportions with
+#' distance). Typically the second element of the list returned by
+#' simlandscapes.
+#' @param pd.rate numeric, the probability of returning to the central place to
+#' "deliver prey" to nest/den/etc. on any given step (given the agent isnot
+#' currently located at the central place.) Must be <1.
+#' @param steps integer, the number of steps the agent should take
+#' @param lambda numeric, the strength of the declining exponental function that controls
+#' how "reluctant" the agent is to move large distances. Must be <1.
+#' @param coef.d numeric, can be factor or proportion representing the relative
+#' strength of the preference to move short steps compared to the strength of
+#' the preference for habitat.
+#' @param coef.r numeric, can be factor or proportion representing the relative
+#' strength of the preference for habitat compared to the strength of
+#' the preference for short steps.
+#' @param matsize integer, the size in number of cells of one side of the square
+#' matrix on which the simulation is being run.
+#' @param ...
+#'
+#' @return Returns the list of positions for the simulated individual.
+#' @export
+#'
+#' @examples
+chooseLoc <- function(hab.prob, pd.rate, steps, lambda, coef.d, coef.r, blank.rast, matsize, ...) {
   move.list <- list(c(0,0)) #establish list to store locations
   for (i in 2:steps) {
     #check last location, if at nest - allow hab-based movement
@@ -21,7 +50,7 @@ choose.loc <- function(r, pd.rate, steps, lambda, coef.d, coef.r, blank.rast, ma
         move.list[[i]] <- list.tmp
       } else {
         list.tmp <- list() #create temp list to store each location per iteration
-        loc <- probsel(r, move.list = move.list, coef.d = coef.d, coef.r = coef.r,
+        loc <- probsel(hab.prob, move.list = move.list, coef.d = coef.d, coef.r = coef.r,
                        blank.rast = blank.rast, matsize = matsize,
                        lambda = lambda)
         #pull the coords out so that we can store as x, y not row, col
@@ -35,12 +64,38 @@ choose.loc <- function(r, pd.rate, steps, lambda, coef.d, coef.r, blank.rast, ma
   return(move.list)
 }
 
-#(based on https://scrogster.wordpress.com/2012/04/22/using-r-for-spatial-sampling-with-selection-probabilities-defined-in-a-raster/)
-probsel <- function(r, lambda, move.list, coef.d, coef.r, blank.rast, matsize, ...){
+
+#' probsel
+#'
+#' Makes probabalistic selections of a cell based on the mixture of two
+#' probability matrices. Not intended to be called directly - used by
+#' `chooseloc`
+#'
+#' (based on https://scrogster.wordpress.com/2012/04/22/using-r-for-spatial-sampling-with-selection-probabilities-defined-in-a-raster/)
+#'
+#' @param hab.prob matrix, the habitat matrix with cell values representing the
+#' habitat-only probabilities (uncorrected for the mixing proportions with
+#' distance). Typically the second element of the list returned by
+#' simlandscapes.
+#' @param lambda numeric, the strength of the declining exponental function that controls
+#' how "reluctant" the agent is to move large distances. Must be <1.
+#' @param move.list list, the current `move.list` being maintained by
+#' `chooseloc`. Used to find the current agent position.
+#' @param coef.d numeric, the relative weight to give the distance preference.
+#' @param coef.r numeric, the relative weight to give the habitat preference.
+#' @param matsize integer, the size in number of cells of one side of the square
+#' matrix on which the simulation is being run.
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+probsel <- function(hab.prob, lambda, move.list, coef.d, coef.r, matsize, ...){
   #get distance probabilities
   d <- makeDistProb(matsize = matsize, position = 2, lambda = lambda)
   #combine distance probs with habitat probs, weighted by mixing coefficients
-  x <- (coef.d * d) + (coef.r * r)
+  x <- (coef.d * d) + (coef.r * hab.prob)
   x <- x/sum(x) #convert back to true probabilities
   samp <- sample(matsize^2, size=1, prob=x)
   idmat <- matrix(1:matsize^2, nrow = matsize, byrow = F) #create an index matirx
@@ -80,16 +135,22 @@ makeDistProb <- function (matsize, position, lambda) {
 
 #' matrixPythagoras
 #'
-#' @param IDmat ID matrix on which to calculate distance, must have cell values
-#' matching 1-D indexing
+#' Uses the Pythagorean theorem to calculate the strightline distance between
+#' two matrix cells.
+#'
+#' @param IDmat matrix, the ID matrix on which to calculate distance, must have
+#' cell values matching 1-D indexing. See examples
 #' @param position1 first positions in distance calc
-#' @param position2 second position in dtance calc
+#' @param position2 second position in disttance calc
 #'
 #' @return returns the pythogorean distance between two coordinates (supplied as
 #'matirx indices)
 #' @export
 #'
 #' @examples
+#' #create an example ID matrix for `IDmat`. Example uses a 100x100 matrix
+#' mat <- matrix(1:10000, nrow = 100)
+#'
 matrixPythagoras <- function(position1, position2, IDmat){
   x <- as.numeric(sqrt(((which(d.mat == position1, arr.ind = T)[,"row"]-
                            which(d.mat == position2, arr.ind = T)[,"row"])^2) +
